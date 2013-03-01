24cdc156a6d849744ae3167ca714dc49b014d81f
chao-zhou <eebread@gmail.com>
2013-03-01 17:30:56 +0800

    Db新增 Save方法
    参数
    1. collection name
    2. object
    3. callback

--- models/db.js
+++ models/db.js
@@ -4,8 +4,35 @@
 var settings = require('../settings');
 var MongoClient = require('mongodb').MongoClient;
 
-exports.open = function(callback){
+function open(callback){
 	MongoClient.connect(settings.dbUrl,function(err,db){
 		callback(err,db);
 	});
+};
+
+exports.open = open;
+
+exports.save = function(collection, object, callback){
+
+	open(function(err,db){
+		if(err){
+			db.close();
+			return callback(err);
+		}
+		
+		db.collection(collection, function(err,collection){
+			if(err){
+				db.close();
+				return callback(err);
+			}
+			
+			//collection.ensureIndex('number',{unique: true});
+			collection.insert(object,{safe: true},function(err,object){
+				db.close();
+				callback(err,object);
+			});
+		});
+	});	
+	
+	
 };
