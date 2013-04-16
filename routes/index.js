7638c1ce6cfb5cf737ae2ce9b039aeeb9dd16e63
chao-zhou <eebread@gmail.com>
2013-04-16 15:20:52 +0800

    增加 location上传 与认证

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 
 var user = require('./user');
-
+var loc = require('./location');
 exports = module.exports = function(app){
 	app.get('/', home);
-	app.get('/users', user.list);
+	app.get('/users', auth, user.list);
 	app.get('/users/reg', user.reg);
 	app.get('/users/login', user.login);
-	app.get('/users/logout', user.logout);
+	app.get('/users/logout', auth, user.logout);
 	
-	app.get('/users/save', user.save);
-	app.get('/users/get', user.get);
-	app.get('/users/update', user.update);
-	app.get('/users/remove', user.remove);
+	app.get('/users/save', auth, user.save);
+	app.get('/users/get', auth, user.get);
+	app.get('/users/update', auth, user.update);
+	app.get('/users/remove', auth, user.remove);
+	
+	app.get('/loc/rec', auth, loc.rec);
 };
 
 
@@ -21,3 +23,12 @@ exports = module.exports = function(app){
 function home(req, res){
 	res.render('index', { title: 'Express' });
 };
+
+function auth(req,res,next){
+	if(req.session && req.session.user){
+		next();
+	}else{
+		next(new Error('Unauthorized'));
+	}
+	
+}
