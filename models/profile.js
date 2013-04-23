dd08ec8ccceb94eb8f7de803a9e974923659c299
chao-zhou <eebread@gmail.com>
2013-04-23 13:51:35 +0800

    添加Model Profile 非DbModel

---
+++ models/profile.js
@@ -0,0 +1,27 @@
+var obj = require('./object');
+
+function Profile(param){
+	var opt = {
+		mail:null,
+		weibo:null,
+		qq:null,
+		homepage:null,
+		company:null,
+		university:null,
+		address:null,
+		job:null
+	};
+	
+	obj.safeCopy(opt,param);
+	
+	this.mail = opt.mail||'';
+	this.weibo = opt.weibo||'';
+	this.qq = opt.qq||'';
+	this.homepage = opt.homepage||'';
+	this.company = opt.company||'';
+	this.university = opt.university||'';
+	this.address = opt.address||'';
+	this.job = opt.job||'';	
+}
+
+module.exports = Profile;
