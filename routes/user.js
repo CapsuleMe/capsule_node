695cd0f633a1812bc301de837c0c46931d5b3af7
chao-zhou <eebread@gmail.com>
2013-04-24 14:11:15 +0800

    数据结构变了。重写User控制，

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,9 @@
 /*
  * GET users listing.
  */
-var crypto = require('crypto');
-var User = require('../models/user');
-var Val = require('./response');
-var Req = require('./request');
+var crypto = require('crypto'), 
+	User = require('../models/user'),
+	Val = require('./value');
 
 
 function cryptoPassword(password){
@@ -13,113 +12,34 @@ function cryptoPassword(password){
 	return md5.update(password).digest('base64');
 }
 
-function getRequestUser(req){
-	return new User({
-		number: req.query.u
-	  , password: cryptoPassword(req.query.p)
-	});
-}
-
 function sendError(res, err){
-	res.send(Val.failed(1,err));
+	res.send(Val.error(1,err));
 }
-
-exports.list = function(req, res){
-  res.send("respond with a resource");
-};
 
 exports.reg = function(req, res){
-	var newUser = getRequestUser(req);	
 
-	User.getByNumber(newUser.number,function(err, user){
-		if(user){
-			err='user already exist';
-		}
-		if(err){
-			return sendError(res,err); 	
-		}
-		
-		newUser.save(function(err,user){
-			if(err){
-				return sendError(res,err); 	
-			}
-			
-			res.send({
-				success:1,
-				err:null,
-				user:user
-			});	
-		});		
-	});	
 };
 
 exports.login = function(req, res){
 
-	//Try to get form session first;
-	User.getByNumber('18600406362',function(err,user){
-		if(err){
-			res.send(err);
-		}
-		
-		if(user){
-			req.session.user = user;
-			res.send(user);
-		}
-	});	
-
-	
 };
 
 exports.logout = function(req, res){
 
 };
 
-
-exports.save = function(req, res){
-	var newUser = getRequestUser(req);
-	newUser.save(function(err,user){
-			if(err){
-				return sendError(res,err); 	
-			}
-			res.send(Val.success(user,'save success'));	
-	});			
-};
-
-exports.get = function(req, res){
-	User.get('513074e555164f7417000001',function(err,user){
-		if(err){
-				return sendError(res,err); 	
-			}
-			
-		res.send(Val.success(user,'get success'));	
-	});
-};
-
-exports.update = function(req, res){
-	User.update('513074e555164f7417000001',{$set:{number:'18611447559'}},function(err,count){
-		if(err){
-				return sendError(res,err); 	
-			}
-			res.send({
-				success:1,
-				err:null,
-				count:count
-			});	
-	});
-};
-
-exports.remove = function(req, res){
-	User.remove('512f2998cb2e3a2c1c000001',function(err,count){
-		if(err){
-				return sendError(res,err); 	
-			}
-			res.send({
-				success:1,
-				err:null,
-				count:count
-			});	
-	});
+exports.remove = function(req,res){
 
 };
+
+exports.update = function(req,res){
+
+};
+
+exports.get = function(req,res){
+
+};
+
+
 
 
