8270640ad1a262b7030c9a8eb0fe0e50c8ba1a28
chao-zhou <eebread@gmail.com>
2013-04-24 13:44:00 +0800

    增加说明
    增加实例的update方法

--- models/creator.js
+++ models/creator.js
@@ -1,5 +1,35 @@
 /**
- * New node file
+ * 这是一个创建Db类的工具，所有的Db工具都应当被这个类初始化。
+ * 被这个类初始化的类，包含一下方法
+ *
+ * 函数上的方法（类似静态）
+ * get(id,callbakc)
+ * 	id = string tpye id value
+ * 	callback = function(err,obj)
+ * 
+ * save(obj,callback)
+ * 	obj = object with same type
+ * 	callback = fucntion(err,obj)
+ *
+ * remove(id,callback)
+ * 	id = string tpye id value
+ * 	callback = function(err,numberOfRemovedDocs)
+ *
+ * update(id,obj,callback)
+ * 	id = string tpye id value
+ * 	obj = object with same type
+ * 	callback = fucntion(err,obj)
+ *
+ *
+ * 势力方法
+ * save(callback)
+ * 	callback = fucntion(err,obj)
+ *
+ * update(callback)
+ * 	callback = fucntion(err,obj)
+ *
+ *
+ *
  */
 var settings = require('../settings');
 var Db = require('./db');
@@ -20,7 +50,9 @@ exports.createModel = function(construct,collection){
 	construct.prototype.save = function(callback){
 		construct.save(this,callback);
 	}
-
+	construct.prototype.update = function(callback){
+		construct.update(this._id,this,callback);
+	}
 };
 
 function save(obj,callback){
