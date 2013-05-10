92f252a6314df537a7ef068c579e78883a54b5f1
chao-zhou <eebread@gmail.com>
2013-05-10 15:58:05 +0800

    实现User API，暂缺 update API的实现，没想好怎么传参数

--- routes/user.js
+++ routes/user.js
@@ -17,27 +17,79 @@ function sendError(res, err){
 }
 
 exports.reg = function(req, res){
-
+	var user = new User({
+		number:req.body.number,
+		password:cryptoPassword(req.body.password)
+	});
+	
+	user.save(function(err,user){
+		if(err){
+			return res.send(Val.error(1,err));
+		}
+		
+		if(!user){
+			return res.send(Val.error(2,'No User'));	
+		}
+		
+		req.session.user = user;
+		res.send(Val.success(user));	
+	});
 };
 
 exports.login = function(req, res){
-
+	var id = req.body.id,
+		pwd = req.body.pwd;
+	User.get(id,function(err,user){
+		if(err){
+			return res.send(Val.error(1,err));
+		}
+		
+		if(!user){
+			return res.send(Val.error(2,'No User'));	
+		}
+			
+		if(user.password != pwd){
+			return res.send(Val.error(2,'Wrong PWD'));	
+		}
+		
+		req.session.user = user;
+		res.send(Val.success(0));	
+	});
 };
 
 exports.logout = function(req, res){
-
+	req.session.user = null;
+	res.send(Val.success(0));	
 };
 
 exports.remove = function(req,res){
-
+	var id = req.body.id;
+	User.remove(id,function(err,numberOfRemovedDocs){
+		if(err){
+			return res.send(Val.error(1,err));
+		}
+		
+		res.send(Val.success(numberOfRemovedDocs));	
+	});
 };
 
 exports.update = function(req,res){
-
+	
 };
 
 exports.get = function(req,res){
-
+	var id = req.body.id;
+	User.get(id,function(err,user){
+		if(err){
+			return res.send(Val.error(1,err));
+		}
+		
+		if(!user){
+			return res.send(Val.error(2,'No User'));	
+		}
+		
+		res.send(Val.success(user));	
+	});
 };
 
 
