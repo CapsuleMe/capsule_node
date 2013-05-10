c5f83570fb880fc3b923a756fca9429bd935233c
chao-zhou <eebread@gmail.com>
2013-05-10 14:36:13 +0800

    添加获取用户个人消息API

--- routes/message.js
+++ routes/message.js
@@ -2,6 +2,8 @@
  * New node file
  */
 var SysMsg = require('../models/sys_msg'),
+	UserMsg = require('../models/user_msg'),
+	FriendMsg = require('../models/friend_msg'),
 	Val = require('./value');
  
 exports.sysmsg = function(req,res){	
@@ -18,5 +20,34 @@ exports.sysmsg = function(req,res){
 			}
 		}
 		res.send(Val.success(ret));
+	});
+};
+
+exports.usermsg = function(req,res){	
+	var ret = [];	
+	var id = req.body.id;
+
+	UserMsg.gets({user:id},function(err,objs){
+		if(err){
+			res.send(Val.error(1,err));
+			return 0;
+		}
+			
+		ret.concat(objs);
+		console.log('1');
+		
+		FriendMsg.gets({to:id},function(err,objs){
+			if(err){
+				res.send(Val.error(1,err));
+				return 0;
+			}
+			
+			ret.concat(objs);
+			console.log('2');
+			
+			res.send(Val.success(ret));
+			console.log('3');
+		});
+		
 	});
 };
