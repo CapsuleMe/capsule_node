50830bceeb01acbbbde280234adb43aa2723724b
chao-zhou <eebread@gmail.com>
2013-05-10 13:51:20 +0800

    新增user message对象

---
+++ models/user_msg.js
@@ -0,0 +1,24 @@
+
+var creator = require('./creator');
+ 	
+function User_Msg(param){
+	var opt = {
+		user:null,
+	 	msg:null,
+	 	time: new Date(),
+	 	active:1
+	}; 
+ 	creator.obj.safeCopy(opt,param);
+ 
+ 	var self = this;
+ 	self._id = param._id;
+ 	selft.user = opt.user;
+ 	self.msg = opt.msg;
+ 	self.time = opt.time;
+ 	self.active = opt.active;
+ }
+ 
+ 
+creator.createModel(User_Msg,'usermsg');
+
+module.exports = User_Msg;
