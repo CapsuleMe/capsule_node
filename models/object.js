e35ca936d8f2777ed4f5b86121f440c6a097a03c
chao-zhou <eebread@gmail.com>
2013-06-14 17:32:44 +0800

    修复SafeCopy第二个参数为空的bug

--- models/object.js
+++ models/object.js
@@ -3,6 +3,10 @@
  */
 // Copy objB value to objA 
 exports.safeCopy = function(objA, objB){
+	if(!objB){
+		return;
+	}
+	
 	for(var p in objA){
 		if(objB[p] || objB[p] == 0){
 			objA[p] = objB[p];	
